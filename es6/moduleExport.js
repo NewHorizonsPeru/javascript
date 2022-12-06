2b20d2e801fe3737cbf12d93edef74a598aa124e
Renato De la Rosa <srdelarosab@gmail.com>
2022-12-05 22:40:15 -0500

    Modules

---
+++ es6/moduleExport.js
@@ -0,0 +1,7 @@
+var API_KEY = "Occaecat veniam nisi fugiat proident occaecat pariatur.";
+var printMessage = (msg) => {
+  console.log(msg);
+};
+
+//export default API_KEY;
+export { API_KEY, printMessage };
---
+++ es6/moduleImport.js
@@ -0,0 +1,4 @@
+import { API_KEY, printMessage } from "./moduleExport.js";
+import * as ModuleX from "./moduleExport.js";
+console.log(ModuleX.API_KEY);
+ModuleX.printMessage("Dolore qui eiusmod nostrud minim tempor aliqua.");
