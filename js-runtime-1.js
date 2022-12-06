3f384b01ef02dccfbd190b00bf1d854f2d69203a
Renato De la Rosa <srdelarosab@gmail.com>
2022-12-05 20:12:30 -0500

    JS Runtime Web API

--- js-runtime-1.js
+++ js-runtime-1.js
@@ -1,5 +1,32 @@
 function printMessage() {
-  console.log("Mensaje: Hello World");
+  console.log("2");
+  setTimeout(function () {
+    console.log("5");
+  }, 5000);
+  console.log("7");
+  setTimeout(function () {
+    console.log("0");
+  }, 0);
 }
-
+// 2 7 0 5
 printMessage();
+
+/** JAVASCRIPT RUNTIME
+ *
+ * CALLSTACK                              CALLBACK QUEUE
+ * **********************                 * ST-5, ST-0 *
+ * *                    *
+ * **********************
+ * *  printMessage()    *
+ * **********************
+ * *  anonymus (window) *                 👀 EVENT LOOP
+ * **********************
+ *
+ * **********************
+ * *                    *
+ * **********************
+ * *                    *
+ * **********************
+ * *                    *
+ * **********************
+ * **/
