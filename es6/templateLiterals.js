3152e372695bdc23b1d42fa1f3542e2f948f18ea
Renato De la Rosa <srdelarosab@gmail.com>
2022-12-05 21:03:04 -0500

    Template Literals

---
+++ es6/templateLiterals.js
@@ -0,0 +1,9 @@
+var firstName = undefined;
+var lastName = "Klein";
+
+console.log(firstName + ", " + lastName);
+
+console.log(`   ${firstName}, 
+${lastName}`);
+
+console.log(`${!firstName ? "Sin Nombre" : firstName}`);
