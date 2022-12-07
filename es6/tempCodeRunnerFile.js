a10ce648165737efad912960384446ecdcd6ba33
Renato De la Rosa <srdelarosab@gmail.com>
2022-12-06 21:07:46 -0500

    Inheritance

--- es6/tempCodeRunnerFile.js
+++ es6/tempCodeRunnerFile.js
@@ -1,10 +1,40 @@
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+class Category {
+  constructor(categoryCode, categoryName) {
+    this.categoryCode = categoryCode;
+    this.categoryName = categoryName;
   }
-  printFullName() {
-    console.log(`${this.firstName}, ${this.lastName}.`);
+  printCategory() {
+    console.log(`${this.categoryCode} - ${this.categoryName}`);
   }
 }
-console.log(typeof Person);
+
+class Product extends Category {
+  constructor(
+    productCode,
+    productName,
+    productPrice,
+    categoryCode,
+    categoryName
+  ) {
+    super(categoryCode, categoryName);
+    this.productCode = productCode;
+    this.productName = productName;
+    this.productPrice = productPrice;
+  }
+
+  printProduct() {
+    console.log(`${this.productCode} - ${this.productName}`);
+  }
+}
+
+const phone = new Product(
+  "9X4S2",
+  "iPhone SE 2022",
+  1999.0,
+  "47X12",
+  "Smartphones"
+);
+
+phone.printProduct();
+phone.printCategory();
+console.log(phone.categoryName);
